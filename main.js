3946c016755f9c6189c6a6c5b103f94d34db874a
Natalia Olmos <60705654+NataliaOlmos@users.noreply.github.com>
2020-11-18 21:10:03 -0600

    Add digital clock

--- main.js
+++ main.js
@@ -23,3 +23,64 @@ darkButton.addEventListener('click', () =>{
 
     localStorage.setItem('theme', colorTheme)
 })
+
+(function(){
+    let updateHour = function(){
+        let fecha = new Date();
+            hora = fecha.getHours(),
+            ampm,
+            minutos = fecha.getMinutes(),
+            segundos = fecha.getSeconds(),
+            diaSemana = fecha.getDay(),
+            dia = fecha.getDate(),
+            mes = fecha.getMonth(),
+            year = fecha.getFullYear();
+        
+        let pHoras = document.getElementById('horas'),
+            pAMPM = document.getElementById('ampm'),
+            pMinutos = document.getElementById('minutos'),
+            pSegundos = document.getElementById('segundos'),
+            pDiaSemana = document.getElementById('diaSemana')
+            pDia = document.getElementById('dia'),
+            pMes = document.getElementById('mes')
+            pYear = document.getElementById('year');
+
+
+        let semana = ['Domingo','Lunes', 'Martes', 'Miércoles','Jueves','Viernes','Sábado'];
+        pDiaSemana.textContent = semana[diaSemana];
+
+        pDia.textContent = dia;
+
+        let meses = ['Enero','Febrero','Marzo','Abril','Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Noviembre','Diciembre']
+        pMes.textContent = meses[mes];
+        pYear.textContent = year;
+
+
+        if(hora >= 12){
+            hora = hora - 12;
+            ampm = 'PM';
+        }else{
+            ampm = 'AM';
+        }
+        if(hora == 0){
+            hora = 12;
+        };
+
+        pHoras.textContent = hora;
+        pAMPM.textContent = ampm;
+
+        if(minutos < 10){minutos="0" + minutos};
+        if(segundos < 10){segundos ="0" +segundos};
+
+        pMinutos.textContent = minutos;
+        pSegundos.textContent= segundos;
+
+
+    
+    }
+
+
+
+    updateHour();
+    let intervalo = setInterval(updateHour, 1000);
+}())
